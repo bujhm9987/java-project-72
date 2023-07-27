2a10c65f444083fdc7fbdcbd3108f28750aef671
Igor Pavlov <bujhm9987@gmail.com>
2023-07-27 20:13:30 +0300

    add Hello World!

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,62 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8000");
+        return Integer.valueOf(port);
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> {
+            ctx.result("Hello World!");
+        });
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateEngine.addTemplateResolver(templateResolver);
+
+        return templateEngine;
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+            JavalinThymeleaf.init(getTemplateEngine());
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
